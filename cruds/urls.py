ae91f3a7241dea2782e0979abebc19eb53f9ad88
anto777molina777@gmail.com <anto777molina777@gmail.com>
2022-08-04 23:10:38 -0500

    upi

--- cruds/urls.py
+++ cruds/urls.py
@@ -3,6 +3,6 @@ from .views import list_crud1, create_task, delete_task
 
 urlpatterns = [
   path('', list_crud1),
-    path('new/', create_task, name='create_task'),
+  path('new/', create_task, name='create_task'),
   path('delete_task/<int:task_id>/', delete_task, name='delete_task'),
 ]
